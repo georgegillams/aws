87d092774754ec4da1d0b7917bf2a3b77cb6c2d5
George Gillams <george.gillams@skyscanner.net>
2020-05-04 08:54:05 +0100

    [NO-JIRA] Separate aws and redis processes

---
+++ pm2_configs/redis/ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [
+    {
+      name: 'redis-server',
+      script: './system_config/start_redis.sh',
+    },
+  ],
+};
--- setup_scripts/5_setup_pm2.sh
+++ setup_scripts/5_setup_pm2.sh
@@ -1,5 +1,7 @@
 pm2 startup | grep sudo | bash
 
+cd /home/ubuntu/aws
+pm2 start /home/ubuntu/aws/pm2_configs/redis/ecosystem.config.js
 cd /home/ubuntu/georgegillams.co.uk
 pm2 start /home/ubuntu/aws/pm2_configs/georgegillams.co.uk/ecosystem.config.js
 # TODO Add other project pm2 scripts here
