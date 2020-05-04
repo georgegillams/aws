618d4736c42d36c8f92f0ca6aad385fc1b5ba6ce
George Gillams <george.gillams@skyscanner.net>
2020-05-04 20:23:47 +0100

    [NO-JIRA] List PM2 gotchas

--- setup_scripts/5_setup_pm2.sh
+++ setup_scripts/5_setup_pm2.sh
@@ -13,3 +13,8 @@ pm2 start /home/ubuntu/aws/pm2_configs/cgwedding/ecosystem.config.js
 
 pm2 save
 
+# PM2 gotchas:
+# - To pick up new environment variables, the process must be deleted and recreated.
+# - If you manually stop a pm2 job, it will no longer be `watching` when you restart it.
+# - If you delete the directory being watched, it will no longer be being watched. Therefore instead delete everything inside.
+
