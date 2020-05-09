5c2601d4a302998c173a574e76b45c7586f51e5f
George Gillams <george.gillams@skyscanner.net>
2020-05-09 15:15:39 +0100

    [NO-JIRA] create empty build directories instead of building projects

--- setup_scripts/4_build_project.sh
+++ setup_scripts/4_build_project.sh
@@ -1,13 +1,13 @@
 cd /home/ubuntu/georgegillams.co.uk
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm ci
-npm run build:aws
+mkdir build
 cd /home/ubuntu/screen-reader-adventures
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm ci
-npm run build:aws
+mkdir build
 cd /home/ubuntu/cgwedding
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm ci
-npm run build:aws
+mkdir build
 cd /home/ubuntu/gg-components
 PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm ci
-npm run build:aws
+mkdir build
 # TODO Add other project redeploys here
