3d513b25b74f821fe955342d850182a7fe90a907
George Gillams <george.gillams@skyscanner.net>
2020-09-20 22:24:27 +0100

    [NO-JIRA] Fix redeployment targets

--- cron_scripts/redeploy_all.sh
+++ cron_scripts/redeploy_all.sh
@@ -2,14 +2,17 @@
 
 cd /home/ubuntu/georgegillams.co.uk
 . /home/ubuntu/aws/cron_scripts/redeploy.sh
+
 cd /home/ubuntu/screen-reader-adventures
 . /home/ubuntu/aws/cron_scripts/redeploy.sh
-cd /home/ubuntu/cgwedding
-. /home/ubuntu/aws/cron_scripts/redeploy.sh
+
 cd /home/ubuntu/beta.cgwedding
 . /home/ubuntu/aws/cron_scripts/redeploy.sh
+
 cd /home/ubuntu/gg-components
 . /home/ubuntu/aws/cron_scripts/redeploy.sh
+
 cd /home/ubuntu/webapp-boilerplate
 . /home/ubuntu/aws/cron_scripts/redeploy.sh
+
 # TODO Add other project redeploys here
