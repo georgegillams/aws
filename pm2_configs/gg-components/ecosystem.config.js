24a6b9102ec3aff8c82f4f6f8f8aefa178c27307
George Gillams <george.gillams@skyscanner.net>
2021-02-27 13:41:47 +0000

    [NO-JIRA] Restart PM2 sites if memory consumption too high

--- pm2_configs/gg-components/ecosystem.config.js
+++ pm2_configs/gg-components/ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'storybook',
-      script: './config/aws/start_aws.sh',
-      watch: ['build'],
+      name: "storybook",
+      script: "./config/aws/start_aws.sh",
+      max_memory_restart: "15M",
+      watch: ["build"],
       // Delay between restart
       watch_delay: 1000,
       ignore_watch: [],
